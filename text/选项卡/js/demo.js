1058b61c4344db8379176784f0ffd072c47420ec
tao893506589 <92136739+tao893506589@users.noreply.github.com>
2021-10-15 20:14:22 +0800

    demo-01

--- text/选项卡/js/demo.js
+++ text/选项卡/js/demo.js
@@ -14,24 +14,25 @@ $(function(){
 
 
     // 鼠标进入左边时  让右边的选项显示
-  var lis = $("#shop .left li");
-  var divs = $("#shop .right div")
-  $(lis).mouseenter(function(){
-      var $num = $(this).index();
-      $("#shop #play").css('display','none')
-      // $("#shop .right div").css('display','none')
-      $("#shop .right div").eq($num).css('display','block').mouseenter(function(){
-          $(this).css('display','block');
-          $("#shop #play").css('display','none')
+    //  虽然动态添加元素   但是在这里才获取元素  此时元素都已经添加上   不需要用到事件委托
+    var lis = $("#shop .left li");
+    var divs = $("#shop .right div")
+    $(lis).mouseenter(function(){
+        var $num = $(this).index();
+        $("#shop #play").css('display','none')
+        // $("#shop .right div").css('display','none')
+        $("#shop .right div").eq($num).css('display','block').mouseenter(function(){
+            $(this).css('display','block');
+            $("#shop #play").css('display','none')
 
-      })
-  }).mouseout(function(){
-      $("#shop #play").css('display','block')
-      $("#shop .right div").css('display','none').mouseout(function(){
-          $(this).css('display','none');
-          $("#shop #play").css('display','block')
-      })
-  })
+        })
+    }).mouseout(function(){
+        $("#shop #play").css('display','block')
+        $("#shop .right div").css('display','none').mouseout(function(){
+            $(this).css('display','none');
+            $("#shop #play").css('display','block')
+        })
+    })
 })
 
 
